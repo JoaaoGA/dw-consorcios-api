48d612b44eb55c0d4b251af0f1c51ea1fcc820a8
Joao GA <joaogabrielalmeida.f@gmail.com>
2025-06-04 15:02:42 -0300

    Corrige export da rota /api/cotacoes

--- pages/api/cotacoes.js
+++ pages/api/cotacoes.js
@@ -1,6 +1,6 @@
-const axios = require('axios');
+import axios from 'axios';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   try {
     const response = await axios.get('https://app.dwconsorcios.com.br/api/v1/contemplados');
     res.status(200).json(response.data);
@@ -8,4 +8,4 @@ module.exports = async (req, res) => {
     console.error('Erro ao buscar cotas:', error.message);
     res.status(500).json({ erro: 'Não foi possível obter os dados.' });
   }
-};
+}
